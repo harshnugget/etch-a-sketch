0ae2af01f7d58d15755e065d5d69b6d0ecaef835
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-13 05:26:50 +0000

    Colour brightness will now gradually decrease
        - Created a changeBrightness function to incrementally decrease RGB values
        - Added functionality to randomize RGB values to apply random colours

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Initialize color to white
+let currentColor = "rgb(255, 255, 255)";
+
 // Variable for size of grid
 const GRID_SIZE = 16;   //(e.g. a size of 16 would create a 16x16 grid)
 
@@ -28,7 +31,44 @@ function buildGrid(size) {
 }
 
 function changeColor(event) {
+    const target = event.target;
+    const color = target.style.backgroundColor;
+
     if (event.buttons === 1) {
-        this.style.backgroundColor = "red";
+        if (color) {
+            changeBrightness(color, target);
+        }
+        else {
+            // Randomize rgb values
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            let randomColor = `rgb(${r}, ${g}, ${b})`;
+
+            // Apply color
+            target.style.backgroundColor = currentColor;
+        }  
     }
+}
+
+function changeBrightness(color, target) {        
+    // Define an amount to decrease each RGB value by
+    const decrementAmount = 10;
+
+    // Use regex to check if background color is RGB format
+    var rgbRegex = /rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)/;
+    if (!rgbRegex.test(color)) {
+        console.log("Invalid color format! Must be RGB.");
+        return;
+    }
+
+    // Get RGB values
+    let rgbValues = color.split("(")[1].replace(")", "").split(",");
+
+    // Decrease RGB values by 10 until 0
+    let r = rgbValues[0] > decrementAmount ? rgbValues[0]-decrementAmount : 0;
+    let g = rgbValues[1] > decrementAmount ? rgbValues[1]-decrementAmount : 0;
+    let b = rgbValues[2] > decrementAmount ? rgbValues[2]-decrementAmount : 0;
+
+    target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
