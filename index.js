a361b6b6b95ba70b32a33f98b159d9b79f9dfffc
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-13 00:26:31 +0000

    Created a function to change color of grid items
        - Grid items will only change colour if hovered over and mouse is held down
        - Adding a "mousedown" event listener fixed an issue with grid items not changing colour when clicked on

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const GRID_SIZE = 16;
+// Variable for size of grid
+const GRID_SIZE = 16;   //(e.g. a size of 16 would create a 16x16 grid)
 
 // Select grid container
 const gridContainer = document.querySelector(".grid-container");
 
-// Create grid columns and rows
-for (let column = 0; column < GRID_SIZE; column++) {
-    let gridColumn = document.createElement("div");
-    gridColumn.classList.add("grid_column");
-    gridContainer.appendChild(gridColumn);
+// Build grid
+buildGrid(GRID_SIZE);
 
-    for (let row = 0; row < GRID_SIZE; row++) {
-        let gridRow = document.createElement("div");
-        gridRow.classList.add("grid_row");
-        gridRow.setAttribute("data-value", `${column*16+row+1}`);
-        gridColumn.appendChild(gridRow);
+function buildGrid(size) {
+    // Create grid columns and rows
+    for (let column = 0; column < size; column++) {
+        let gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid_column");
+        gridContainer.appendChild(gridColumn);
+    
+        for (let row = 0; row < size; row++) {
+            let gridRow = document.createElement("div");
+            gridRow.classList.add("grid_row");
+            gridRow.setAttribute("data-value", `${column*16+row+1}`);
+            gridColumn.appendChild(gridRow);
+            
+            // Event listeners to change color of grid items
+            gridRow.addEventListener("mouseover", changeColor);
+            gridRow.addEventListener("mousedown", changeColor);
+        }
+    }
+}
+
+function changeColor(event) {
+    if (event.buttons === 1 || event.type === "mousedown") {
+        this.style.backgroundColor = "red";
     }
 }
